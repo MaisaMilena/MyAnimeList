2b9558dab60ea2b3e5aa9b3f3bee7c91b37ad2bb
Maisa <maisa.milena@gmail.com>
2018-10-02 22:53:15 -0300

    fixing data duplication when running the app for the first time

--- MyAnimeList/MyAnimeList/Anime management/RealmDatabase.swift
+++ MyAnimeList/MyAnimeList/Anime management/RealmDatabase.swift
@@ -24,13 +24,30 @@ final class RealmDatabase: AnimeDatabase {
 
     init(realm: Realm) {
         self.realm = realm
+        let saikiKusuo = Anime(title: "Saiki Kusuo no psi-nan",
+                               description: "To the average person, psychic abilities might seem a blessing; for Kusuo Saiki, however, this couldn't be further from the truth",
+                               rating: 5,
+                               status: .toWatch)
+        
+        let melancoly = Anime(title: "The Melancholy of Haruhi Suzumiya",
+                              description: "Haruhi Suzumiya was about this girl named The Melancholy, who was desperately trying to convince herself that she was living inside of an anime, and that she was secretly god",
+                              rating: 5,
+                              status: .watched)
+        
+        let steins = Anime(title: "Steins;Gate",
+                           description: "Adapted from the critically acclaimed visual novel by 5pb. and Nitroplus, Steins;Gate takes Okabe through the depths of scientific theory and practicality. Forced across the diverging threads of past and present, Okabe must shoulder the burdens that come with holding the key to the realm of time",
+                           rating: 5,
+                           status: .watched)
+        add(anime: saikiKusuo)
+        add(anime: melancoly)
+        add(anime: steins)
     }
 
     func add(anime: Anime) -> Bool {
         let status: ()? = try? realm.write {
             realm.add(anime.encode())
         }
-        return status != nil
+        return status == nil
     }
 
     func getAnimes(_ status: AnimeStatusType) -> [Anime] {

2c5308c27c3ea844a1086d7c3c4ab4a1ea0db66b
Maisa <maisa.milena@gmail.com>
2018-10-09 09:34:23 -0300

    adding Resources for new design: colors and fonts

---
+++ MyAnimeList/MyAnimeList/Resources/PaleteColors.swift
@@ -0,0 +1,16 @@
+//
+//  PaleteColors.swift
+//  MyAnimeList
+//
+//  Created by Maisa on 09/10/18.
+//  Copyright © 2018 Maisa Milena. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let primaryBlue = UIColor(named: "PrimaryBlue")
+    static let secondaryGray = UIColor(named: "SecondaryGray")
+    
+}
